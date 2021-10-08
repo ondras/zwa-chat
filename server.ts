5abd4e3cff4b1859826bf6fa8eb5075db5e49815
Ondřej Žára <ondrej.zara@gmail.com>
2021-10-08 14:00:52 +0200

    first

---
+++ server.ts
@@ -0,0 +1,46 @@
+let sockets = new Set<WebSocket>();
+const channel = new BroadcastChannel("chat");
+
+function distribute(data: string, sender?: WebSocket) {
+	sockets.forEach(socket => {
+		if (socket != sender) { socket.send(data); }
+	});
+}
+function handleRequest(request: Request) {
+	let upgrade = request.headers.get("upgrade") || "";
+	if (upgrade.toLowerCase() != "websocket") {
+		console.error("failed to accept websocket for url", request.url);
+		return new Response("this is a websocket endpoint");
+	}
+
+	const ip = request.headers.get("x-forwarded-for");
+	console.log("new websocket connection from", ip);
+
+	const { socket, response } = Deno.upgradeWebSocket(request);
+
+	sockets.add(socket);
+
+	socket.addEventListener("message", (event: MessageEvent) => {
+		const { data } = event;
+		console.log("received socket message", data);
+		distribute(data, socket);
+		channel.postMessage(data);
+	});
+
+	socket.addEventListener("close", _ => {
+		console.log("client disconnected");
+		sockets.delete(socket);
+	});
+
+	return response;
+}
+
+addEventListener("fetch", (event: FetchEvent) => {
+	event.respondWith(handleRequest(event.request));
+});
+
+channel.addEventListener("message", (event: MessageEvent) => {
+	const { data } = event;
+	console.log("received broadcast message", data);
+	distribute(data);
+});
